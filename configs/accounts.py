40b3a5e2c7c056be9a889c565db83478e74e18fa
Lakshmikanth Pandre <pandrel@amazon.com>
2022-06-07 14:17:41 -0500

    added stage account and vpc details

--- configs/accounts.py
+++ configs/accounts.py
@@ -23,7 +23,7 @@ managed_accounts = {
             "vpc_id": "vpc-0d619b30b1b1a7d5d",
             "private_subnets": [ "subnet-0d74610f5afc452af", "subnet-0861cf1cdd11a238f" ],
             "database_subnets": [ "subnet-035594a067c6f24ca", "subnet-084601862baff95d1" ],
-            
+
         },
         "consumer": {
             "enabled": True,
@@ -36,13 +36,29 @@ managed_accounts = {
     },
     "stage" : {
         "ingestion": {
-            "account": '111111111111', "region": default_region, "enabled": False
+            "enabled": True,
+            "account": '399536370415', 
+            "region": default_region, 
+            "vpc_id": "vpc-0800e033adb7345a8",
+            "private_subnets": [ "subnet-031bfed79ae10d607", "subnet-0dd28c752371698ee" ],
+            "database_subnets": [ "subnet-0f090d336b925a08f", "subnet-04250fe703c275ede" ],
         },
         "datalake": {
-            "account": '222222222222', "region": default_region, "enabled": False
+            "enabled": True,
+            "account": '497145807131', 
+            "region": default_region, 
+            "vpc_id": "vpc-0d0ce93c3dad427f1",
+            "private_subnets": [ "subnet-0b59d8c1c43d71ddb", "subnet-0ae1b1f2a46ac2a8d" ],
+            "database_subnets": [ "subnet-017cd909f6a8ff42f", "subnet-09aba4b84d837c121" ],
+            
         },
         "consumer": {
-            "account": '333333333333', "region": default_region, "enabled": False
+            "enabled": True,
+            "account": '027102085951', 
+            "region": default_region, 
+            "vpc_id": "vpc-0deb9caa4936f0829",
+            "private_subnets": [ "subnet-0407c99282d8f3d68", "subnet-03aeea327d921c539" ],
+            "database_subnets": [ "subnet-02744649d7b87d7bd", "subnet-0518fa695a922087a" ],
         }
     },
     "prod" : {
