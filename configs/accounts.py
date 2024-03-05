4918126394d2c2118df911afa91d945db909cb0f
EC2 Default User <ec2-user@ip-172-31-66-17.ec2.internal>
2024-03-05 17:37:22 +0000

    init code

--- configs/accounts.py
+++ configs/accounts.py
@@ -3,14 +3,14 @@ from configs.globalconfig import default_region
 
 
 ## Devops Account
-devops_account = { "account": '594801937661', "region": default_region }
+devops_account = { "account": 'xxxxxx', "region": default_region }
 
 ## CI/CD managed Accounts
 managed_accounts = {
     "dev" : {
 
             "enabled": True,
-            "account": '594801937661', 
+            "account": 'xxxxxx', 
             "region": default_region, 
             "vpc_id": "vpc-c50f3bbf",
             "private_subnets": [ "subnet-8de942c0", "subnet-465ebb19" ],
@@ -18,7 +18,7 @@ managed_accounts = {
     "prod" : {
 
             "enabled": True,
-            "account": '594801937661', 
+            "account": 'xxxxxx', 
             "region": default_region, 
             "vpc_id": "vpc-c50f3bbf",
             "private_subnets": [ "subnet-8de942c0", "subnet-465ebb19" ],
